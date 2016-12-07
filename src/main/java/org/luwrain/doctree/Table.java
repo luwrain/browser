71ce8a111a224cdd46f2ef6b64f4509914615470
Michael Pozhidaev <michael.pozhidaev@gmail.com>
2016-12-08 03:12:02 +0700

    Using empty lines in empty table cells

--- src/main/java/org/luwrain/doctree/Table.java
+++ src/main/java/org/luwrain/doctree/Table.java
@@ -1,19 +1,3 @@
-/*
-   Copyright 2012-2016 Michael Pozhidaev <michael.pozhidaev@gmail.com>
-   Copyright 2015-2016 Roman Volovodov <gr.rPman@gmail.com>
-
-   This file is part of the LUWRAIN.
-
-   LUWRAIN is free software; you can redistribute it and/or
-   modify it under the terms of the GNU General Public
-   License as published by the Free Software Foundation; either
-   version 3 of the License, or (at your option) any later version.
-
-   LUWRAIN is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   General Public License for more details.
-*/
 
 package org.luwrain.doctree;
 
@@ -86,23 +70,6 @@ n.subnodes[0].parentNode = n;
 	}
 	return count;
     }
-
-    /*
-    public boolean isSingleLineRow(int index)
-    {
-	for(Node n: subnodes[index].subnodes)
-	{
-	    if (n.subnodes.length > 1)
-		return false;
-	    if (n.subnodes[0].type != Node.Type.PARAGRAPH || !(n.subnodes[0] instanceof Paragraph))
-		return false;
-	    final Paragraph p = (Paragraph)n.subnodes[0];
-	    if (p.getNodeHeight() > 1)
-		return false;
-	}
-	    return true;
-    }
-    */
 
     public boolean isSingleCellTable()
     {
--- src/main/java/org/luwrain/doctree/TableRow.java
+++ src/main/java/org/luwrain/doctree/TableRow.java
@@ -1,19 +1,3 @@
-/*
-   Copyright 2012-2016 Michael Pozhidaev <michael.pozhidaev@gmail.com>
-   Copyright 2015-2016 Roman Volovodov <gr.rPman@gmail.com>
-
-   This file is part of the LUWRAIN.
-
-   LUWRAIN is free software; you can redistribute it and/or
-   modify it under the terms of the GNU General Public
-   License as published by the Free Software Foundation; either
-   version 3 of the License, or (at your option) any later version.
-
-   LUWRAIN is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   General Public License for more details.
-*/
 
 package org.luwrain.doctree;
 
@@ -47,15 +31,13 @@ public class TableRow extends Node
     void addEmptyCells(int num)
     {
 	NullCheck.notNullItems(subnodes, "subnodes");
-	//	Log.debug("table", "has " + subnodes.length + ", required " + num);
 	if (subnodes.length >= num)
 	    return;
 	final Node[] newNodes = Arrays.copyOf(subnodes, num);
 	for(int i = subnodes.length;i < newNodes.length;++i)
 	{
-	    //	    Log.debug("table", "adding new");
 	    final TableCell cell = new TableCell();
-	    cell.subnodes = new Node[]{NodeFactory.newPara("-")};
+	    cell.subnodes = new Node[]{NodeFactory.newEmptyLine()};
 	    cell.subnodes[0].parentNode = cell;
 	    cell.parentNode = this;
 	    newNodes[i] = cell;
