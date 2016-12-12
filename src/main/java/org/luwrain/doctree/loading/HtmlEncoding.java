5af827a091dc7268b852161b7c699ef8bc175592
Michael Pozhidaev <michael.pozhidaev@gmail.com>
2016-12-12 16:34:49 +0300

    Minor fixes

--- src/main/java/org/luwrain/doctree/loading/HtmlEncoding.java
+++ src/main/java/org/luwrain/doctree/loading/HtmlEncoding.java
@@ -33,11 +33,11 @@ class HtmlEncoding
     static String getEncoding(Path path) throws IOException
     {
 	NullCheck.notNull(path, "path");
-		        //  html5 <meta charset="UTF-8">
+	//  html5 <meta charset="UTF-8">
         //  html4 <meta http-equiv="Content-Type"
 	//content="text/html;charset=ISO-8859-1">
         //doc.updateMetaCharsetElement()
-			final Document doc = Jsoup.parse(Files.newInputStream(path), "US-ASCII", path.toString());
+	final Document doc = Jsoup.parse(Files.newInputStream(path), "US-ASCII", path.toString());
         final Elements el = doc.getElementsByTag("meta");
         for(Element e: el)
 	{
@@ -47,25 +47,25 @@ class HtmlEncoding
 	    final String httpEquiv = e.attr("http-equiv");
 	    if (httpEquiv == null || !httpEquiv.trim().toLowerCase().equals("content-type"))
 		continue;
-	    	    final String content = e.attr("content");
+	    final String content = e.attr("content");
 	    if(content == null || content.isEmpty())
 		continue;
 	    final MimeType mime;
-	    	try {
-mime = new MimeType(content);
-	}
-	catch(MimeTypeParseException ex)
-	{
-	    ex.printStackTrace();
-	    continue;
-	}
-			    final String baseType = mime.getBaseType();
+	    try {
+		mime = new MimeType(content);
+	    }
+	    catch(MimeTypeParseException ex)
+	    {
+		ex.printStackTrace();
+		continue;
+	    }
+	    final String baseType = mime.getBaseType();
 	    if (baseType == null || !baseType.trim().toLowerCase().equals("text/html"))
 		continue;
-	    	    final String res = mime.getParameter("charset");
-		    if (res != null && !res.trim().isEmpty())
-			return res.trim();
-	    	}
+	    final String res = mime.getParameter("charset");
+	    if (res != null && !res.trim().isEmpty())
+		return res.trim();
+	}
 	return "";
     }	
 }
