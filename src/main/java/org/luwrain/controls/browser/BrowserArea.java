d6c2ac31b03fbea7465a605d53ad61c8df2815f0
Michael Pozhidaev <michael.pozhidaev@gmail.com>
2017-06-29 07:31:12 +0700

    Cleaning BrowserArea

--- src/main/java/org/luwrain/controls/browser/BrowserArea.java
+++ src/main/java/org/luwrain/controls/browser/BrowserArea.java
@@ -29,17 +29,14 @@ import org.luwrain.doctree.*;
 
 public class BrowserArea extends DoctreeArea
 {
+    static private final String LOG_COMPONENT = "browser";
+
     protected final Callback callback;
     protected final DocumentBuilder documentBuilder;
     protected final Browser browser;
-
-    private Document doc=null;
-
+    protected Document doc=null;
     protected Events.State state = Events.State.READY;
     protected int progress = 0;
-
-    //    protected final Vector<HistoryElement> elementHistory = new Vector<HistoryElement>();
-    protected boolean complexMode = false;
 
     public BrowserArea(ControlEnvironment context, Browser browser,
 		       Callback callback, ClientThread clientThread, DocumentBuilder documentBuilder,
@@ -68,7 +65,6 @@ public class BrowserArea extends DoctreeArea
     {
 	final long t1=new Date().getTime();
 	browser.rescanDom();
-	complexMode = false;
 	updateView();
 	return true;
     }
@@ -76,7 +72,7 @@ public class BrowserArea extends DoctreeArea
     protected void updateView()
     {
    	final int x=getHotPointX();
-final int y=getHotPointY();
+	final int y=getHotPointY();
 	doc = documentBuilder.build(browser);
 	//   	page.setWatchNodes(builder.watch);
 	doc.commit();
@@ -116,27 +112,16 @@ final int y=getHotPointY();
     {
 	if (isEmpty())
 	    return false;
-	Log.debug("browser", "trying to cancel loading");
 	browser.stop();
 	return true;
     }
 
-    @Override public boolean onAreaQuery(AreaQuery query)
-    {
-	return false;
-    }
-
     @Override public String getAreaName()
     {
-	return browser.getTitle()+" "+state.name()+" "+progress;
+	final String title = browser.getTitle();
+	return title != null?title:"";
     }
 
-    @Override protected boolean onSpace(KeyboardEvent event)
-    {
-    	onClick();
-    	return super.onSpace(event);
-    }
-    
     @Override public boolean onEnvironmentEvent(EnvironmentEvent event)
     {
 	NullCheck.notNull(event, "event");
@@ -150,141 +135,9 @@ final int y=getHotPointY();
 	}
     }
 
-public boolean onHistoryPrev()
-	{
-    	browser.executeScript("history.go(-1);");
-		return true;
-	}
-
-    public void toggleVisibility()
-    {
-browser.setVisibility(!browser.getVisibility());
-    }
-
-
-    /*
-	protected boolean onBackspace()
-    {
-	if(elementHistory.isEmpty()) return false;
-	HistoryElement h=elementHistory.lastElement();
-	elementHistory.remove(elementHistory.size()-1);
-	complexMode=h.mode;
-	//current = h.element;
-	updateView();
-	return true;
-    }
-    */
-
-    /**Performs some default action relevant to the element under the hot
-     * point. What the exact action will be done is dependent on the type of the
-     * element under the hot point. If there is a form input edit, the user
-     * will get an offer to enter some text. If there is a form list, the
-     * user will get a popup to choose some item from the list and so
-     * on. This operation may be performed only if the browser is free
-     * (meaning, not empty and not busy).
-     *
-     * @return true if the operation has been done, false otherwise (usually the browser is busy or doesn't have a loaded page)
-     */
-    protected boolean onClick()
-    {
-	/*
-    	if (isEmpty() || isBusy())
-    	    return false;
-    	Run run=this.getCurrentRun();
-    	if(run instanceof TextRun)
-    	{
-    		Object t=((TextRun)run).getAssociatedObject();
-    		if(t==null) return false;
-    		if(t instanceof ElementAction)
-    		{
-    			ElementAction action=(ElementAction)t;
-    			Boolean result=null;
-    			switch(action.type)
-    			{
-    				case UNKNOWN:
-					case CLICK:
-						result = emulateClick(action.element);
-						break;
-					case EDIT:
-						result = onFormEditText(action.element);
-						break;
-					case SELECT:
-						result = onFormSelectFromList(action.element);
-						break;
-					default:
-						Log.error("web-browser","unknown action type: "+action.type.name());
-						return false;
-    			}
-				((Actions)callback).skipNextContentChangedNotify();
-				page.doFastUpdate();
-    		} else
-    		{
-    			Log.error("web-browser","current TextRun have unknown associated object, type "+t.getClass());
-    		}
-    	}
-    	//System.out.println(run.getClass());
-	*/
-    	return false;
-    }
-
-    /**Asks the browser core to emulate the action which looks like the user
-     * clicks on the given element. This operation may be performed only if
-     * the browser is free (meaning, not empty and not busy).
-     *
-     * @param el The element to emulate click on
-     * @return true if the operation has been done, false otherwise (usually the browser is busy or doesn't have a loaded page)
-     */
-    protected boolean emulateClick(BrowserIterator el)
-    {
-	NullCheck.notNull(el, "el");
-	if (isEmpty())
-	    return false;
-	el.emulateClick();
-	return true;
-    }
-    protected boolean emulateSubmit(BrowserIterator el)
-    {
-	NullCheck.notNull(el, "el");
-	if (isEmpty())
-	    return false;
-	el.emulateSubmit();
-	return true;
-    }
-
-    protected boolean onFormEditText(BrowserIterator el)
-    {
-	NullCheck.notNull(el, "el");
-	if (isEmpty())
-	    return false;
-	final String oldValue = el.getText();
-	final String newValue = callback.askFormTextValue(oldValue != null?oldValue:"");
-	if (newValue == null) 
-	    return true;
-	el.setText(newValue);
-	updateView();
-	return true;
-    }
-
-    protected boolean onFormSelectFromList(BrowserIterator el)
-    {
-	NullCheck.notNull(el, "el");
-	if (isEmpty())
-	    return false;
-	final String[] items = el.getMultipleText();
-	if (items == null || items.length==0) 
-	    return true; // FIXME:
-	final String res = callback.askFormListValue(items, true);
-	if (res == null)
-	    return true;
-	el.setText(res);
-	updateView();
-	return true;
-    }
-
-    protected void onPageChangeState(Events.State state)
+    void onPageChangeState(Events.State state)
     {
 	NullCheck.notNull(state, "state");
-	Log.debug("browser", "new page state:" + state);
 	this.state = state;
 	switch(state)
 	{
@@ -293,7 +146,6 @@ browser.setVisibility(!browser.getVisibility());
 	    return;
 	case SUCCEEDED:
 	    refresh();
-	    goToStartPage();
 	    callback.onBrowserSuccess(browser.getTitle());
 	    return;
 	case FAILED:
@@ -304,28 +156,23 @@ browser.setVisibility(!browser.getVisibility());
 	case SCHEDULED:
 	    return;
 	default:
-	    Log.warning("browser", "unexpected new page state:" + state);
+	    Log.warning(LOG_COMPONENT, "unexpected new page state:" + state);
 	}
     }
-
-    private void goToStartPage()
-	{
-    	this.onMoveHotPoint(new MoveHotPointEvent(0,0,false));
-	}
 
     void onContentChanged()
     {
     	refresh();
-		callback.onBrowserContentChanged(browser.getLastTimeChanged());
+	callback.onBrowserContentChanged(browser.getLastTimeChanged());
     }
 
-void onProgress(Number progress)
+    public void onProgress(Number progress)
     {
 	NullCheck.notNull(progress, "progress");
 	this.progress = (int)(progress==null?0:Math.floor(progress.doubleValue()*100));
     }
 
-    protected void onDownloadStart(String url)
+    public void onDownloadStart(String url)
     {
 	//FIXME:
     }
