b3025a27f44b99ec41db0481cf70dff6271729b4
Michael Pozhidaev <michael.pozhidaev@gmail.com>
2018-07-13 10:53:36 +0700

    Fixing selectors.Selector

--- src/main/java/org/luwrain/browser/selectors/Selector.java
+++ src/main/java/org/luwrain/browser/selectors/Selector.java
@@ -1,5 +1,5 @@
 /*
-   Copyright 2012-2017 Michael Pozhidaev <michael.pozhidaev@gmail.com>
+   Copyright 2012-2018 Michael Pozhidaev <michael.pozhidaev@gmail.com>
    Copyright 2015-2016 Roman Volovodov <gr.rPman@gmail.com>
 
    This file is part of LUWRAIN.
@@ -15,127 +15,95 @@
    General Public License for more details.
 */
 
+//LWR_API 1.0
+
 package org.luwrain.browser.selectors;
 
 import org.luwrain.core.*;
 import org.luwrain.browser.*;
 
+
+/**
+ * An abstract selector for manipulation of {@link BrowserIterator}
+ * states. During processing of the web-page, several types of filters
+ * could be needed (for example, to filter only visible elements).  This
+ * class implements general navigation procedures, such as jumping to the
+ * next or previous element, calling the abstract method {@code suits()}
+ * forexamining if the particular element is suitable or not.
+ */
 public abstract class Selector
 {
-abstract public boolean suits(BrowserIterator list);
+    abstract public boolean suits(BrowserIterator it);
 
     /** Moves the iterator to the first element, approved by the {@code suits()} 
-     * method. If there is no such element, the method restored 
-     * the original state of the iterator.
-     *
-     * @param list The iterator to move
-     * @return True if the iterator gets necessary position, false otherwise
-     */
-    //    boolean moveFirst(ElementIterator list);
-
-    /** Moves the iterator to the next element, approved by the {@code suits()} 
-     * method. If there is no such element, the method restored 
-     * the original state of the iterator.
-     *
-     * @param list The iterator to move
-     * @return True if the iterator gets necessary position, false otherwise
-     */
-    //    boolean moveNext(ElementIterator list);
-
-    /** Moves the iterator to the previous element, approved by the {@code suits()} 
-     * method. If there is no such element, the method restored 
-     * the original state of the iterator.
-     *
-     * @param list The iterator to move
-     * @return True if the iterator gets necessary position, false otherwise
-     */
-    //    boolean movePrev(ElementIterator list);
-
-    //    boolean moveToPos(ElementIterator list, int pos);
-
-
-
-    /** Moves the iterator to the first element, approved by the {@code suits()} 
-     * method. If there is no such element, the method restored 
-     * the original state of the iterator.
+     * method. If there is no such element, the method restores 
+     * the iterator's original state.
      *
      * @param it The iterator to move
-     * @return True if the iterator gets necessary position, false otherwise
+     * @return True if the iterator gets the necessary position, false otherwise
      */
-public boolean moveFirst(BrowserIterator it)
+    public boolean moveFirst(BrowserIterator it)
     {
 	final int origState = it.getPos();
 	final int count = it.getBrowser().numElements();
+	if (count == 0)
+	    return false;
 	it.setPos(0);
 	while(it.getPos() < count && !suits(it)) 
-		it.setPos(it.getPos()+1);
+	    it.setPos(it.getPos()+1);
 	if(it.getPos() >= count)
 	{
-		it.setPos(origState);
+	    it.setPos(origState);
 	    return false;
 	}
 	return true;
     }
 
     /** Moves the iterator to the next element, approved by the {@code suits()} 
-     * method. If there is no such element, the method restored 
-     * the original state of the iterator.
+     * method. If there is no such element, the method restores 
+     * the iterator's original state.
      *
      * @param it The iterator to move
-     * @return True if the iterator gets necessary position, false otherwise
+     * @return True if the iterator gets the necessary position, false otherwise
      */
-public boolean moveNext(BrowserIterator it)
+    public boolean moveNext(BrowserIterator it)
     {
 	final int origState = it.getPos();
 	final int count = it.getBrowser().numElements();
+	if (count == 0 || it.getPos() + 1 >= count)
+	    return false;
 	it.setPos(it.getPos()+1);
 	while(it.getPos() < count && !suits(it)) 
-		it.setPos(it.getPos()+1);
+	    it.setPos(it.getPos()+1);
 	if(it.getPos() >= count)
 	{
-		it.setPos(origState);
+	    it.setPos(origState);
 	    return false;
 	}
 	return true;
     }
 
     /** Moves the iterator to the previous element, approved by the {@code suits()} 
-     * method. If there is no such element, the method restored 
-     * the original state of the iterator.
+     * method. If there is no such element, the method restores 
+     * the iterator's original state.
      *
      * @param it The iterator to move
-     * @return True if the iterator gets necessary position, false otherwise
+     * @return True if the iterator gets the necessary position, false otherwise
      */
-public boolean movePrev(BrowserIterator it)
+    public boolean movePrev(BrowserIterator it)
     {
 	final int origState = it.getPos();
+	final int count = it.getBrowser().numElements();
+	if (count == 0 || it.getPos() == 0)
+	    return false;
 	it.setPos(it.getPos()-1);
 	while(it.getPos() >= 0 && !suits(it)) 
-		it.setPos(it.getPos()-1);
+	    it.setPos(it.getPos() - 1);
 	if(it.getPos()<0)
 	{
-		it.setPos(origState);
+	    it.setPos(origState);
 	    return false;
 	}
 	return true;
-    }
-
-public boolean moveToPos(BrowserIterator it, int pos)
-    {
-    	if(it.getPos() == pos)
-    		return true;
-    	else if(it.getPos() < pos)
-	    {
-		while(moveNext(it)) 
-		    if(pos == it.getPos()) 
-			return true;
-		return false;
-	    } else
-	    {
-		while(movePrev(it)) 
-		    if(pos == it.getPos()) 
-			return true;
-		return false;
-	    }
     }
 }
