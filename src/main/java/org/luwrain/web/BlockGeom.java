0058131d282b1df38f6727bd3cb416af34e1d624
Michael Pozhidaev <msp@luwrain.org>
2024-04-01 13:51:49 +0700

    Adding BlockGeomTest

--- src/main/java/org/luwrain/web/BlockGeom.java
+++ src/main/java/org/luwrain/web/BlockGeom.java
@@ -22,22 +22,52 @@ import static org.luwrain.util.RangeUtils.*;
 
 public final class BlockGeom
 {
-    private final Block[] blocks;
+    public final Block[] blocks;
+    private final int count;
+    private int[][] matrix = null;
     public BlockGeom(Block[] blocks)
     {
 	this.blocks = blocks;
+	this.count = blocks.length;
     }
 
     	public void process()
 	{
 	    Arrays.sort(blocks);
+	    buildMatrix();
+	    for(int i = 0;i < count;i++)
+		for(int j = i + 1;j < count;j++)
+		{
+		    final Block b1 = blocks[i], b2 = blocks[j];
+		    final int space = matrix[i][j];
+		    if (space < 0)//Completely independent blocks
+			continue;
+		    b2.top = Math.max(b2.top, b1.top + b1.height + space - 1);
+		}
 	}
+
+    private void buildMatrix()
+    {
+    this.matrix = new int[count][count];
+    for(int i = 0;i < count;i++)
+	for(int j = 0;j < count;j++)
+	    matrix[i][j] = -1;
+    for(int i = 0;i < count;i++)
+	for(int j = i + 1;j < count;j++)
+	{
+	    final Block b1 = blocks[i], b2 = blocks[j];
+	    if (!intersects(b1.left, b1.right - b1.left, b2.left, b2.right - b1.left))
+		continue;
+	    if (b1.top == b2.top)
+		matrix[i][j] = 1; else
+		matrix[i][j] = b2.top - b1.top;
+	    assert matrix[i][j] >= 0;
+	}
+}
 
     static public class Block implements Comparable<Block>
     {
-	public int left, right, top, height;
-
-
+	protected int left, right, top, height;
 	@Override public  int compareTo(Block b)
 	{
 	    if (top != b.top)
---
+++ src/test/java/org/luwrain/web/BlockGeomTest.java
@@ -0,0 +1,68 @@
+/*
+   Copyright 2012-2024 Michael Pozhidaev <msp@luwrain.org>
+
+   This file is part of LUWRAIN.
+
+   LUWRAIN is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 3 of the License, or (at your option) any later version.
+
+   LUWRAIN is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+*/
+
+package org.luwrain.web;
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BlockGeomTest
+{
+    @Test public void main()
+    {
+	final var b = new BlockGeom(new BlockGeom.Block[]{
+				new Block(1, 4, 3, 5),
+		new Block(3, 5, 1, 5),
+		new Block(1, 5, 1, 5),
+	    });
+	b.process();
+	assertNotNull(b.blocks);
+	assertEquals(3, b.blocks.length);
+
+	final var b1 = b.blocks[0];
+		final var b2 = b.blocks[1];
+			final var b3 = b.blocks[2];
+
+	//The first block
+	assertEquals(1, b1.left);
+	assertEquals(5, b1.right);
+		assertEquals(1, b1.top);
+				assertEquals(5, b1.height);
+
+				//The second block
+					assertEquals(3, b2.left);
+	assertEquals(5, b2.right);
+		assertEquals(6, b2.top);
+				assertEquals(5, b2.height);
+
+								//The third block
+					assertEquals(1, b3.left);
+	assertEquals(4, b3.right);
+			assertEquals(7, b3.top);
+				assertEquals(5, b3.height);
+    }
+
+    static final class Block extends BlockGeom.Block
+    {
+	Block(int left, int right, int top, int height)
+	{
+	    this.left = left;
+	    this.right = right;
+	    this.top = top;
+	    this.height = height;
+	}
+    }
+}
