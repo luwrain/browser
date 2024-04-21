2f5f1251dbb150af0d01d4842ead458bdccd405f
Michael Pozhidaev <msp@luwrain.org>
2024-04-21 06:41:34 +0300

    Testing lines for TextAppearanceTest

--- src/test/java/org/luwrain/app/browser/TextAppearanceTest.java
+++ src/test/java/org/luwrain/app/browser/TextAppearanceTest.java
@@ -30,6 +30,18 @@ public class TextAppearanceTest
     @Test public void main()
     {
 	final var view = new View(new Appearance(), Arrays.asList(getTestBlocks()));
+	assertEquals(9, view.getLineCount());
+	for(int i = 0;i < view.getLineCount();i++)
+	    assertEquals(25, view.getLine(i).length());
+	assertEquals("                         ", view.getLine(0));
+	assertEquals(" Строка 1                ", view.getLine(1));
+	assertEquals(" Строка 2      Блок 3    ", view.getLine(2));
+	assertEquals("               Строка 2  ", view.getLine(3));
+	assertEquals(" Блок 2        Строка 3  ", view.getLine(4));
+	assertEquals(" Строка 2                ", view.getLine(5));
+	assertEquals("               Блок 4    ", view.getLine(6));
+	assertEquals("               Строка 2  ", view.getLine(7));
+	assertEquals("               Строка 3  ", view.getLine(8));
     }
 
     static final class Appearance implements BlockArea.Appearance
